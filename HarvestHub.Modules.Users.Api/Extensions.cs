b056c0a49c20fd6bea6d0a98b9c4733f05f84ffd
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-27 12:52:01 +0200

    Add dbContext Configuration

---
+++ HarvestHub.Modules.Users.Api/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HarvestHub.Modules.Users.Api
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddUsersModule(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ HarvestHub.Modules.Users.Dal/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using HarvestHub.Shared.Database.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using HarvestHub.Shared;
+using Microsoft.EntityFrameworkCore;
+using HarvestHub.Modules.Users.Dal.Persistance;
+
+namespace HarvestHub.Modules.Users.Dal
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var options = configuration.GetOptions<SqlOptions>(SqlOptions.SectionName);
+            services.AddDbContext<UsersDbContext>(ctx =>
+                ctx.UseSqlServer(options.ConnectionString));
+
+            return services;
+        }
+    }
+}
