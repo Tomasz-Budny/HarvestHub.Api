8caad5bc95ae9e7fa8080aac4c982eabc6af91b7
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-27 12:31:50 +0200

    Add Users dbContext

---
+++ HarvestHub.Modules.Users.Dal/Persistance/Configurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using HarvestHub.Modules.Users.Dal.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HarvestHub.Modules.Users.Dal.Persistance.Configurations
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Email).IsUnique();
+            builder.Property(x => x.Email).IsRequired();
+            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.LastName).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ HarvestHub.Modules.Users.Dal/Persistance/UsersDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HarvestHub.Modules.Users.Dal.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HarvestHub.Modules.Users.Dal.Persistance
+{
+    internal sealed class UsersDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("users");
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
