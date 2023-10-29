6dd163bf630d7687dd8ab40b67998e8de948e692
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-29 13:42:08 +0100

    Add Field aggregate, add aggregate and entity primitives to shared module

---
+++ HarvestHub.Modules.Fields.Core/Fields/Aggregates/Field.cs
@@ -0,0 +1,12 @@
+﻿using HarvestHub.Modules.Fields.Core.Fields.ValueObjects;
+using HarvestHub.Shared.Primitives;
+
+namespace HarvestHub.Modules.Fields.Core.Fields.Aggregates
+{
+    public class Field : AggregateRoot<FieldId>
+    {
+        public Field(FieldId id) : base(id)
+        {
+        }
+    }
+}
---
+++ HarvestHub.Modules.Fields.Core/Fields/ValueObjects/FieldId.cs
@@ -0,0 +1,14 @@
+﻿namespace HarvestHub.Modules.Fields.Core.Fields.ValueObjects
+{
+    public record FieldId
+    {
+        public Guid Value { get; set; }
+
+        public FieldId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator Guid(FieldId userId) => userId.Value;
+    }
+}
