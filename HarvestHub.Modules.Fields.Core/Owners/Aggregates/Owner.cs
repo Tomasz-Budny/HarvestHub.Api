9279bc62a3cff3a62b104a7f9ce727f1c6742c44
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-29 21:28:31 +0100

    Add FirstName and LastName valueObjects

--- HarvestHub.Modules.Fields.Core/Owners/Aggregates/Owner.cs
+++ HarvestHub.Modules.Fields.Core/Owners/Aggregates/Owner.cs
@@ -1,10 +1,14 @@
-﻿using HarvestHub.Modules.Fields.Core.SharedKernel.ValueObjects;
+﻿using HarvestHub.Modules.Fields.Core.Owners.ValueObjects;
+using HarvestHub.Modules.Fields.Core.SharedKernel.ValueObjects;
 using HarvestHub.Shared.Primitives;
 
 namespace HarvestHub.Modules.Fields.Core.Owners.Aggregates
 {
     public class Owner : AggregateRoot<OwnerId>
     {
+        public FirstName FirstName { get; set; }
+        public LastName LastName { get; set; }
+        
         public Owner(OwnerId id) : base(id)
         {
         }
---
+++ HarvestHub.Modules.Fields.Core/Owners/ValueObjects/LastName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarvestHub.Modules.Fields.Core.Owners.ValueObjects
+{
+    internal class LastName
+    {
+    }
+}
