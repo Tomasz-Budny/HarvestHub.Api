1090802db9e05cbf9a901b211b07f0b77baeb3c3
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-30 22:31:32 +0100

    Add FieldRepository with implementation

---
+++ HarvestHub.Modules.Fields.Core/Fields/Repositories/IFieldRepository.cs
@@ -0,0 +1,9 @@
+﻿using HarvestHub.Modules.Fields.Core.Fields.Aggregates;
+
+namespace HarvestHub.Modules.Fields.Core.Fields.Repositories
+{
+    public interface IFieldRepository
+    {
+        Task AddAsync (Field field);
+    }
+}
---
+++ HarvestHub.Modules.Fields.Infrastructure/Persistance/Repositories/FieldRepository.cs
@@ -0,0 +1,24 @@
+﻿using HarvestHub.Modules.Fields.Core.Fields.Aggregates;
+using HarvestHub.Modules.Fields.Core.Fields.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace HarvestHub.Modules.Fields.Infrastructure.Persistance.Repositories
+{
+    internal class FieldRepository : IFieldRepository
+    {
+        private readonly FieldsDbContext _dbContext;
+        private readonly DbSet<Field> _fields;
+
+        public FieldRepository(FieldsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _fields = dbContext.Fields;
+        }
+
+        public async Task AddAsync(Field field)
+        {
+            await _fields.AddAsync(field);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
