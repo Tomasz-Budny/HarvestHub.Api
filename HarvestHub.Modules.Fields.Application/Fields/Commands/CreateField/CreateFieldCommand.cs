80b03500421d32048f8180b5b4ca19a9f1b518a5
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-30 19:05:54 +0100

    Add foo implementation of createField command

---
+++ HarvestHub.Modules.Fields.Application/Fields/Commands/CreateField/CreateFieldCommand.cs
@@ -0,0 +1,6 @@
+﻿using HarvestHub.Shared.Messaging;
+
+namespace HarvestHub.Modules.Fields.Application.Fields.Commands.CreateField
+{
+    internal record CreateFieldCommand(Guid FieldId, string Name) : ICommand;
+}
---
+++ HarvestHub.Modules.Fields.Application/Fields/Commands/CreateField/CreateFieldCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using HarvestHub.Shared.Messaging;
+
+namespace HarvestHub.Modules.Fields.Application.Fields.Commands.CreateField
+{
+    internal class CreateFieldCommandHandler : ICommandHandler<CreateFieldCommand>
+    {
+        public Task Handle(CreateFieldCommand request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("It is working!");
+            return Task.CompletedTask;
+        }
+    }
+}
