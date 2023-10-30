ab89b99faf99a124e78b8084d115572144213255
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-30 12:32:32 +0100

    Refactor Vertex to Entity add insertVertices method to field aggregate

--- HarvestHub.Modules.Fields.Core/Fields/Aggregates/Field.cs
+++ HarvestHub.Modules.Fields.Core/Fields/Aggregates/Field.cs
@@ -1,4 +1,5 @@
-﻿using HarvestHub.Modules.Fields.Core.Fields.ValueObjects;
+﻿using HarvestHub.Modules.Fields.Core.Fields.Entities;
+using HarvestHub.Modules.Fields.Core.Fields.ValueObjects;
 using HarvestHub.Modules.Fields.Core.SharedKernel.ValueObjects;
 using HarvestHub.Shared.Primitives;
 
@@ -12,12 +13,35 @@ namespace HarvestHub.Modules.Fields.Core.Fields.Aggregates
         public Area Area { get; set; }
         public Class Class { get; set; }
         public Address Address { get; set; }
-        public IReadOnlyList<Vertex> Vertices => _vertices.AsReadOnly();
 
-        protected List<Vertex> _vertices = new();
+        protected LinkedList<Vertex> _vertices = new();
 
         public Field(FieldId id) : base(id)
         {
+        }
+
+        public void InsertVertices(LinkedList<Vertex> insertVertices)
+        {
+            LinkedListNode<Vertex>? currentNode = _vertices.First;
+            LinkedListNode<Vertex>? currInsertNode = insertVertices.First;
+            uint orderCount = 0;
+
+            while (currentNode != null)
+            {
+                currentNode.Value.Order = orderCount;
+                while (currInsertNode is not null &&
+                    currentNode is not null &&
+                    currInsertNode.Value.Order == currentNode.Value.Order)
+                {
+                    _vertices.AddBefore(currentNode, currInsertNode.Value);
+                    orderCount++;
+                    currInsertNode = currInsertNode.Next;
+                    currentNode.Value.Order = orderCount;
+                }
+
+                orderCount++;
+                currentNode = currentNode.Next;
+            }
         }
     }
 }
---
+++ HarvestHub.Modules.Fields.Core/Fields/ValueObjects/VertexId.cs
@@ -0,0 +1,16 @@
+﻿namespace HarvestHub.Modules.Fields.Core.Fields.ValueObjects
+{
+    public record VertexId
+    {
+        public Guid Value { get; }
+
+        public VertexId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator VertexId(string value) => new(value);
+
+        public static implicit operator Guid(VertexId vertexId) => vertexId.Value;
+    }
+}
