a94e0216b3ef8163ae15b123c43a6695d23661e7
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-02 13:17:11 +0100

    Add CreateField event with foo handler

---
+++ HarvestHub.Modules.Fields.Application/Owners/EventHandlers/FieldCreatedHandler.cs
@@ -0,0 +1,13 @@
+﻿using HarvestHub.Modules.Fields.Shared.Events;
+using HarvestHub.Shared.Events;
+
+namespace HarvestHub.Modules.Fields.Application.Owners.EventHandlers
+{
+    internal sealed class FieldCreatedHandler : IEventHandler<FieldCreated>
+    {
+        public Task HandleAsync(FieldCreated @event, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HarvestHub.Modules.Fields.Shared/Events/FieldCreated.cs
@@ -0,0 +1,6 @@
+﻿using HarvestHub.Shared.Events;
+
+namespace HarvestHub.Modules.Fields.Shared.Events
+{
+    public record FieldCreated(string Name, double Area) : IEvent;
+}
