6414383186d8ca1d50a494731ea4cc71eb3647e2
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-03 18:14:26 +0100

    Add Fertilization history record entity

---
+++ HarvestHub.Modules.Fields.Core/Fields/Entities/FertilizationHistoryRecord.cs
@@ -0,0 +1,23 @@
+﻿using HarvestHub.Modules.Fields.Core.Fields.Primitives;
+using HarvestHub.Modules.Fields.Core.Fields.ValueObjects;
+
+namespace HarvestHub.Modules.Fields.Core.Fields.Entities
+{
+    public class FertilizationHistoryRecord : HistoryRecord
+    {
+        public FertilizerType FertilizerType { get; }
+        public Amount Amount { get; }
+        public FertilizationHistoryRecord(
+            HistoryRecordId id, 
+            FieldId fieldId, 
+            DateTime date,
+            FertilizerType fertilizerType,
+            Amount amount
+
+        ) : base(id, fieldId, date)
+        {
+            FertilizerType = fertilizerType;
+            Amount = amount;
+        }
+    }
+}
---
+++ HarvestHub.Modules.Fields.Core/Fields/Primitives/FertilizerType.cs
@@ -0,0 +1,13 @@
+﻿namespace HarvestHub.Modules.Fields.Core.Fields.Primitives
+{
+    public enum FertilizerType
+    {
+        Unknown = 0,
+        Compost = 1,
+        Manure = 2,
+        AmmoniumNitrate = 3,
+        Nitrogen = 4,
+        Phosphates = 5,
+        Potassium = 6
+    }
+}
