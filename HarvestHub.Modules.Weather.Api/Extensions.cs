3fb688a8204d942928df4ef919408d7fd594b113
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-15 22:57:36 +0100

    Register new module in dependency injection container

---
+++ HarvestHub.Modules.Weather.Api/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HarvestHub.Modules.Weather.Api
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddWeatherModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
